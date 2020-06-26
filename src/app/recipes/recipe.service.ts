85af17203aea58a0feffeeee8ad133e799656a33
vianney diris <vianney.diris@gmail.com>
2020-06-26 14:09:11 +0200

    add list ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,24 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Recipe 1', 'this is a test for a recipe 1', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-    new Recipe('Recipe 2', 'this is a test for a recipe 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+    new Recipe('Tasty Burger',
+    'This is a very good burger.',
+    'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
+     [new Ingredient('buns', 1),
+      new Ingredient('steak', 1),
+      new Ingredient('tomatoes', 2)
+    ]),
+    new Recipe('French Sandwich',
+    'French food are the best',
+    'https://cdn.pixabay.com/photo/2014/09/18/21/17/sandwich-451403_1280.jpg',
+    [ new Ingredient('baguette', 1),
+      new Ingredient('ham', 3),
+      new Ingredient('tomatoes', 2)
+    ])
   ];
 
   getRecipe(){
