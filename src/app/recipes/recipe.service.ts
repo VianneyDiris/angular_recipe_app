4fe63eec8cfd26a58e60ffe585ca5aef68fe5563
vianney diris <vianney.diris@gmail.com>
2020-06-26 10:35:18 +0200

    add link between recipeService and component

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,8 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService{
+  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe('Recipe 1', 'this is a test for a recipe 1', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
     new Recipe('Recipe 2', 'this is a test for a recipe 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
@@ -9,4 +11,5 @@ export class RecipeService{
   getRecipe(){
     return this.recipes.slice();
   }
+
 }
